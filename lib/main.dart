8ef66f8fbc70d1c27304610ad8fe1007bc7d14e8
Damodar Lohani <dlohani48@gmail.com>
2018-12-04 22:03:47 +0545

    changing to stateful widget setup

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,14 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+    State<StatefulWidget> createState() {
+      return _MyAppState();
+    }
+}
 
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
