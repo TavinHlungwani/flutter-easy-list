297f01413e26ff381d167f862e97dc5eb786010f
Damodar Lohani <dlohani48@gmail.com>
2018-12-05 17:52:49 +0545

    hardware and toolbar backbutton access

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class ProductPage extends StatelessWidget {
   final Map<String,String> product;
@@ -7,27 +8,34 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(product['title'])
-      ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(product['imageUrl']),
-            Container(padding: EdgeInsets.all(8.0), child: Text(product['title'])),
-            Container(
-              padding: EdgeInsets.all(8.0),
-              child: RaisedButton(
-                color: Theme.of(context).accentColor,
-                child: Text('Delete'),
-                onPressed: () => Navigator.pop(context, true),
-              ),
-            )
-          ],
+    return WillPopScope(
+      onWillPop: () {
+        print('Back button pressed');
+        Navigator.pop(context,false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text(product['title'])
+          ),
+          body: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(product['imageUrl']),
+                Container(padding: EdgeInsets.all(8.0), child: Text(product['title'])),
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: RaisedButton(
+                    color: Theme.of(context).accentColor,
+                    child: Text('Delete'),
+                    onPressed: () => Navigator.pop(context, true),
+                  ),
+                )
+              ],
+            ),
+          )
         ),
-      )
-    );
+      );
   }
 }
