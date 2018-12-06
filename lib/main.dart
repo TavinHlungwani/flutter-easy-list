882a01d1801fdd00ce9204eef6f3a53005178c3a
Damodar Lohani <dlohani48@gmail.com>
2018-12-06 07:14:44 +0545

    on unknown route

--- lib/main.dart
+++ lib/main.dart
@@ -60,6 +60,9 @@ class _MyAppState extends State<MyApp> {
         }
         return null;
       },
+      onUnknownRoute: (RouteSettings settings){
+        return MaterialPageRoute(builder: (BuildContext context)=> ProductsPage(_products,_addProduct, _deleteProduct));
+      },
     );
   }
 }
