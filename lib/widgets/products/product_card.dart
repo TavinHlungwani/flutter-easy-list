31e96c4f32bbd38b152be83e0ffa439181f30fff
Damodar Lohani <dlohani48@gmail.com>
2018-12-07 14:38:13 +0545

    extracting product card widget

---
+++ lib/widgets/products/product_card.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import './price_tag.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    Key key,
+    @required this.product,
+    @required this.index
+  }) : super(key: key);
+
+  final Map<String, dynamic> product;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset(product['imageUrl']),
+          Container(
+            margin: EdgeInsets.only(top: 8.0),
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      product['title'],
+                      style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, fontFamily: 'Oswald'),
+                    ),
+                  ),
+                  SizedBox(width: 8.0,),
+                  PriceTag(product['price'].toString())
+                ],
+              ),
+            ) 
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.info),
+                color: Theme.of(context).accentColor,
+                onPressed: () => Navigator.pushNamed<bool>(context,'/products/' + index.toString()),
+              ),
+              IconButton(
+                icon: Icon(Icons.favorite_border),
+                color: Colors.red,
+                onPressed: () {},
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/products/products.dart
+++ lib/widgets/products/products.dart
@@ -1,61 +1,17 @@
 import 'package:flutter/material.dart';
 
-import './price_tag.dart';
+import './product_card.dart';
 
 class Products extends StatelessWidget {
   final List<Map<String,dynamic>> products;
 
   Products({this.products});
 
-  Widget _buildProductItem(BuildContext context, int index) {
-    return Card(
-      child: Column(
-        children: <Widget>[
-          Image.asset(products[index]['imageUrl']),
-          Container(
-            margin: EdgeInsets.only(top: 8.0),
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    child: Text(
-                      products[index]['title'],
-                      style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, fontFamily: 'Oswald'),
-                    ),
-                  ),
-                  SizedBox(width: 8.0,),
-                  PriceTag(products[index]['price'].toString())
-                ],
-              ),
-            ) 
-          ),
-          ButtonBar(
-            alignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.info),
-                color: Theme.of(context).accentColor,
-                onPressed: () => Navigator.pushNamed<bool>(context,'/products/' + index.toString()),
-              ),
-              IconButton(
-                icon: Icon(Icons.favorite_border),
-                color: Colors.red,
-                onPressed: () {},
-              )
-            ],
-          )
-        ],
-      ),
-    );
-  }
-
   Widget _buildProductList() {
     Widget productCards;
     if(products.length > 0) {
         productCards = ListView.builder(
-        itemBuilder: _buildProductItem,
+        itemBuilder: (BuildContext context, int index) => ProductCard(product: products[index], index: index),
         itemCount: products.length,
       );
     }else {
